215cb1b8cbaa1ade93ed86949489b181666f5da6
Alejandro Alvarez Martinez <alex@Alex-air.local>
2015-08-13 12:17:13 -0400

    Initial commit

---
+++ .zshrc
@@ -0,0 +1,101 @@
+# uncomment to profile prompt startup with zprof
+# zmodload zsh/zprof
+
+# history
+SAVEHIST=100000
+
+# vim bindings
+bindkey -v
+
+
+fpath=( "$HOME/.zfunctions" $fpath )
+
+
+# antigen time!
+source ~/code/antigen/antigen.zsh
+
+
+######################################################################
+### install some antigen bundles
+
+local b="antigen-bundle"
+
+
+# Don't load the oh-my-zsh's library. Takes too long. No need.
+	# antigen use oh-my-zsh
+
+# Guess what to install when running an unknown command.
+$b command-not-found
+
+# Helper for extracting different types of archives.
+$b extract
+
+# atom editor
+$b atom
+
+# homebrew  - autocomplete on `brew install`
+$b brew
+$b brew-cask
+
+# Tracks your most used directories, based on 'frecency'.
+$b robbyrussell/oh-my-zsh plugins/z
+
+# suggestion as you type
+$b tarruda/zsh-autosuggestions
+
+# nicoulaj's moar completion files for zsh
+# $b zsh-users/zsh-completions src
+
+# Syntax highlighting on the readline
+$b zsh-users/zsh-syntax-highlighting
+
+# colors for all files!
+$b trapd00r/zsh-syntax-highlighting-filetypes
+
+# dont set a theme, because pure does it all
+$b mafredri/zsh-async
+$b sindresorhus/pure
+
+# history search
+$b zsh-users/zsh-history-substring-search
+
+# Tell antigen that you're done.
+antigen apply
+
+###
+#################################################################################################
+
+
+
+# bind UP and DOWN arrow keys for history search
+zmodload zsh/terminfo
+bindkey "$terminfo[kcuu1]" history-substring-search-up
+bindkey "$terminfo[kcud1]" history-substring-search-down
+
+export PURE_GIT_UNTRACKED_DIRTY=0
+
+# Automatically list directory contents on `cd`.
+auto-ls () {
+	emulate -L zsh;
+	# explicit sexy ls'ing as aliases arent honored in here.
+	hash gls >/dev/null 2>&1 && CLICOLOR_FORCE=1 gls -aFh --color --group-directories-first || ls
+}
+chpwd_functions=( auto-ls $chpwd_functions )
+
+# history mgmt
+# http://www.refining-linux.org/archives/49/ZSH-Gem-15-Shared-history/
+setopt inc_append_history
+setopt share_history
+
+
+zstyle ':completion:*' matcher-list '' 'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
+
+
+# uncomment to finish profiling
+# zprof
+
+
+
+# Load default dotfiles
+source ~/.bash_profile
+
---
+++ setup-a-new-machine.sh
@@ -0,0 +1,201 @@
+# copy paste this file in bit by bit.
+# don't run it.
+  echo "do not run this script in one go. hit ctrl-c NOW"
+  read -n 1
+
+
+##############################################################################################################
+###  backup old machine's key items
+
+mkdir -p ~/migration/home
+cd ~/migration
+
+# what is worth reinstalling?
+brew leaves      		> brew-list.txt    # all top-level brew installs
+brew cask list 			> cask-list.txt
+npm list -g --depth=0 	> npm-g-list.txt
+
+
+# then compare brew-list to what's in `brew.sh`
+#   comm <(sort brew-list.txt) <(sort brew.sh-cleaned-up)
+
+# let's hold on to these
+
+cp ~/.extra ~/migration/home
+cp ~/.z ~/migration/home
+
+cp -R ~/.ssh ~/migration/home
+cp -R ~/.gnupg ~/migration/home
+
+cp /Library/Preferences/SystemConfiguration/com.apple.airport.preferences.plist ~/migration  # wifi
+
+cp ~/Library/Preferences/net.limechat.LimeChat.plist ~/migration
+
+cp -R ~/Library/Services ~/migration # automator stuff
+
+cp -R ~/Documents ~/migration
+
+cp ~/.bash_history ~/migration # back it up for fun?
+
+cp ~/.gitconfig.local ~/migration
+
+cp ~/.z ~/migration # z history file.
+
+# sublime text settings
+cp "~/Library/Application Support/Sublime Text 3/Packages" ~/migration
+
+
+# iTerm settings.
+  # Prefs, General, Use settings from Folder
+
+# Finder settings and TotalFinder settings
+#   Not sure how to do this yet. Really want to.
+
+# Timestats chrome extension stats
+#   chrome-extension://ejifodhjoeeenihgfpjijjmpomaphmah/options.html#_options
+# 	gotta export into JSON through devtools:
+#     copy(JSON.stringify(localStorage, null, '  '))
+#     pbpaste > timestats-canary.json.txt
+
+# Current Chrome tabs via OneTab
+
+# software licenses like sublimetext
+
+
+### end of old machine backup
+##############################################################################################################
+
+
+
+##############################################################################################################
+### XCode Command Line Tools
+#      thx  https://github.com/alrra/dotfiles/blob/c2da74cc333/os/os_x/install_applications.sh#L39
+
+if [ $(xcode-select -p &> /dev/null; printf $?) -ne 0 ]; then
+    xcode-select --install &> /dev/null
+    # Wait until the XCode Command Line Tools are installed
+    while [ $(xcode-select -p &> /dev/null; printf $?) -ne 0 ]; do
+        sleep 5
+    done
+	xcode-select -p &> /dev/null
+	if [ $? -eq 0 ]; then
+        # Prompt user to agree to the terms of the Xcode license
+        # https://github.com/alrra/dotfiles/issues/10
+       sudo xcodebuild -license
+   fi
+fi
+###
+##############################################################################################################
+
+
+
+##############################################################################################################
+### homebrew!
+
+# (if your maching has /usr/local locked down (like google's), you can do this to place everything in ~/.homebrew
+mkdir $HOME/.homebrew && curl -L https://github.com/mxcl/homebrew/tarball/master | tar xz --strip 1 -C $HOME/.homebrew
+export PATH=$HOME/.homebrew/bin:$HOME/.homebrew/sbin:$PATH
+
+# install all the things
+./brew.sh
+./brew-cask.sh
+
+### end of homebrew
+##############################################################################################################
+
+
+
+
+##############################################################################################################
+### install of common things
+###
+
+# github.com/jamiew/git-friendly
+# the `push` command which copies the github compare URL to my clipboard is heaven
+bash < <( curl https://raw.github.com/jamiew/git-friendly/master/install.sh)
+
+
+# Type `git open` to open the GitHub page or website for a repository.
+npm install -g git-open
+
+
+# github.com/rupa/z   - oh how i love you
+git clone https://github.com/rupa/z.git ~/code/z
+chmod +x ~/code/z/z.sh
+# consider reusing your current .z file if possible. it's painful to rebuild :)
+# z is hooked up in .bash_profile
+
+
+# github.com/thebitguru/play-button-itunes-patch
+# disable itunes opening on media keys
+git clone https://github.com/thebitguru/play-button-itunes-patch ~/code/play-button-itunes-patch
+
+
+# my magic photobooth symlink -> dropbox. I love it.
+# 	 + first move Photo Booth folder out of Pictures
+# 	 + then start Photo Booth. It'll ask where to put the library.
+# 	 + put it in Dropbox/public
+# 	* Now… you can record photobooth videos quickly and they upload to dropbox DURING RECORDING
+# 	* then you grab public URL and send off your video message in a heartbeat.
+
+
+# for the c alias (syntax highlighted cat)
+sudo easy_install Pygments
+
+
+# change to bash 4 (installed by homebrew)
+BASHPATH=$(brew --prefix)/bin/bash
+sudo echo $BASHPATH >> /etc/shells
+chsh -s $BASHPATH # will set for current user only.
+echo $BASH_VERSION # should be 4.x not the old 3.2.X
+# Later, confirm iterm settings aren't conflicting.
+
+
+# iterm with more margin! http://hackr.it/articles/prettier-gutter-in-iterm-2/
+#   (admittedly not as easy to maintain)
+
+
+# setting up the sublime symlink
+ln -sf "/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl" ~/bin/subl
+
+
+###
+##############################################################################################################
+
+
+
+##############################################################################################################
+### remaining configuration
+###
+
+# go read mathias, paulmillr, gf3, alraa's dotfiles to see what's worth stealing.
+
+# prezto and antigen communties also have great stuff
+#   github.com/sorin-ionescu/prezto/blob/master/modules/utility/init.zsh
+
+# set up osx defaults
+#   maybe something else in here https://github.com/hjuutilainen/dotfiles/blob/master/bin/osx-user-defaults.sh
+sh .osx
+
+# setup and run Rescuetime!
+
+###
+##############################################################################################################
+
+
+
+##############################################################################################################
+### symlinks to link dotfiles into ~/
+###
+
+#   move git credentials into ~/.gitconfig.local    	http://stackoverflow.com/a/13615531/89484
+#   now .gitconfig can be shared across all machines and only the .local changes
+
+# symlink it up!
+./symlink-setup.sh
+
+###
+##############################################################################################################
+
+
+
